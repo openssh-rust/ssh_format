6b83185f41b34d69fae337ff55ac85985f795cdc
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-05 12:02:22 +1100

    Optimize `Error`: Use `Box<str>` instead of `String`
    For the former is smaller and does not allocate unused heap memory.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Error {
-    Message(String),
+    Message(Box<str>),
     Eof,
     InvalidBoolEncoding,
     InvalidChar,
@@ -18,13 +18,13 @@ pub enum Error {
 
 impl ser::Error for Error {
     fn custom<T: Display>(msg: T) -> Self {
-        Error::Message(msg.to_string())
+        Error::Message(msg.to_string().into_boxed_str())
     }
 }
 
 impl de::Error for Error {
     fn custom<T: Display>(msg: T) -> Self {
-        Error::Message(msg.to_string())
+        Error::Message(msg.to_string().into_boxed_str())
     }
 }
 
